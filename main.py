bedeb913599c0bd9f8b88317c643fe8c0ab071bd
Mathieu Mescam <mate@e.email>
2023-07-15 14:49:02 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import csv
+from datetime import datetime, timedelta
+from icalendar import Calendar, Event, vRecur
+
+def create_ics_file(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        cal = Calendar()
+
+        for row in reader:
+            birthdate, name = row[0], row[1]
+            event = Event()
+
+            # Parse birthdate from the CSV
+            birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()
+
+            # Set event details
+            event.add('summary', f'Birthday: {name}')
+            event.add('dtstart', birthdate)
+            event.add('dtend', birthdate)
+
+            # Create a repeating rule for annual recurrence
+            rrule = vRecur()
+            rrule['freq'] = 'yearly'
+            rrule['interval'] = 1
+
+            # Set the rule as the event's recurrence rule
+            event.add('rrule', rrule)
+
+            # Add event to calendar
+            cal.add_component(event)
+
+    return cal
+
+def save_ics_file(cal, output_file):
+    with open(output_file, 'wb') as file:
+        file.write(cal.to_ical())
+
+    print(f"ICS file '{output_file}' created successfully!")
+
+if __name__ == '__main__':
+    csv_file = 'data/dates.csv'
+    output_file = 'data/birthdays.ics'
+
+    calendar = create_ics_file(csv_file)
+    save_ics_file(calendar, output_file)
+
